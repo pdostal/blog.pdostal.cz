275677743bea693a3ae48c93aac5f40291700945
Pavel Dostál <quick@vDeb.pdostal.cz>
2013-07-05 10:51:55 +0200

    So functions.php is clear.

--- functions.php
+++ functions.php
@@ -1,16 +1,26 @@
 <?php
-    function the_slug() { $post_data = get_post($post->ID, ARRAY_A); $slug = $post_data['post_name']; return $slug; }
+    function the_slug() {
+        $post_data = get_post($post->ID, ARRAY_A);
+        $slug = $post_data['post_name'];
+        return $slug;
+    }
 
     function my_function_admin_bar() { return false; }
-    add_filter( 'show_admin_bar' , 'my_function_admin_bar');
+    add_filter('show_admin_bar' , 'my_function_admin_bar');
 
     function new_excerpt_more($more) { return '...'; }
     add_filter( 'excerpt_more', 'new_excerpt_more' );
     function custom_excerpt_length($length) { return 999; }
-    add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+    add_filter('excerpt_length', 'custom_excerpt_length', 999);
 
-	register_sidebar(array( 'name' => 'Sidebar', 'id' => 'sidebar', 'before_widget' => '<section class="widget" id="%2$s">', 'after_widget' => '</section>', 'before_title' => '<h1>', 'after_title' => '</h1>' ));
+	register_sidebar(array(
+        'name' => 'Sidebar', 'id' => 'sidebar',
+        'before_widget' => '<section class="widget" id="%2$s">', 'after_widget' => '</section>',
+        'before_title' => '<h1>', 'after_title' => '</h1>'
+    ));
 
-    function register_my_menus() { register_nav_menus( array('headermenu' => __('Header Menu'))); }
-	add_action( 'init', 'register_my_menus' );
- ?>
+    function register_my_menus() {
+        register_nav_menus(array('headermenu' => __('Header Menu')));
+    }
+	add_action('init', 'register_my_menus');
+?>
