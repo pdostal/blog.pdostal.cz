70d772791f5534bf27b45051f05b07e25535ed74
quiiicq <git@pdostal.cz>
2013-07-06 06:36:48 +0200

    Upgraded singe.php

--- single.php
+++ single.php
@@ -25,36 +25,15 @@
 <section id="main">
 <?php if (have_posts()) { while (have_posts()) { the_post(); ?>
 	<article>
-	<?php if(!has_tag('simpleheader', get_the_ID())) { ?>
 		<header>
 			<span class="date"><?php the_time('j. n. Y v G:m') ?></span>
 			<h1><a href="<?php the_permalink(""); ?>"><?php the_title(); ?></a></h1>
-			<div class="line"></div>
-			<span class="author">Vydal/a: <?php the_author_posts_link(); ?></span>
 			<span class="category">Kategorie: <?php the_category(', '); ?></span>
 			<span class="tags"><?php the_tags('', '', ''); ?></span>
 		</header>
-	<?php } else { ?>
-		<div class="counter"><ul>
-			<li class="previousarticle tags"><?php previous_post_link('<span>%link</span>', '« Předchozí'); ?></li>
-			<li class="nextarticle tags"><?php next_post_link('<span>%link</span>', 'Další »'); ?></li>
-		</ul></div>
-	<?php } ?>
 		<div class="content"><?php the_content(__('(Celý článek...)')); ?></div>
-		<?php if (get_the_author_meta('user_login') == 'e-Mail') { ?>
-		<div class="disclaimer">
-			<p>Text tohoto příspěvku je uveřejněný na základě výzvy a to bez jakékoliv obsahové, či gramatické úpravy.
-			Obsah tohoto příspěvku nemusí vyjadřovat názorové postoje členů občanského sdružení CZcampuseros a odpovídá za něj výlučně autor,
-			kterého identita může představovat mj. obecně používanou přezdívku na sociálních sítích a nebyla ze strany CZcampuseros nijak ověřována,
-			mimo kontroly existence e-mail adresy, ze které byl příspěvek odeslán.</p>
-		</div>
-		<?php } comments_template( '', true ); ?>
+		<?php comments_template( '', true ); ?>
 		<div class="comment_form"><?php comment_form($args); ?></div>
-		<div class="counter"><ul>
-			<li class="previous tags"><?php previous_post_link('<span>%link</span>', '« « «'); ?></li>
-			<li class="dot tags"><span>&nbsp;</span></li>
-			<li class="next tags"><?php next_post_link('<span>%link</span>', '» » »'); ?></li>
-		</ul></div>
 	</article>
 <?php } } else { ?>
 	<article>
