d87a4249bed99b19b4c35667422df84858a7144f
quiiicq <git@pdostal.cz>
2013-07-11 09:00:21 +0200

    Mobile aside styled.

--- footer.php
+++ footer.php
@@ -1,11 +1,14 @@
 		<div class="clear"></div>
 		<footer>
-			<span>
+			<span class="viewswitcher">
 				<a href="?screentype=<?php if($screentype == 'mobile') { echo 'desktop'; } else { echo 'mobile'; } ?>">
 				<?php if($screentype == 'mobile') { echo 'Klasické'; } else { echo 'Mobilní'; } ?> zobrazení</a> | 
 			</span>
-			<span>
+			<span class="wordpressfooter">
 				Powered by <a href="http://wordpress.org/">Wordpress</a>
+			</span>
+			<div class="clear"></div>
+			<span class="authorfooter">
 				&copy; <a itemprop="url" href="http://pdostal.cz/">Pavel Dostál</a>
 				(<a href="mailto:pdostal@pdostal.cz">pdostal@pdostal.cz</a>)
 			</span>
