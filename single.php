86665a878dc00bbbcdf365b1c488e67523f33a78
pdostal <git@pdostal.cz>
2014-01-05 12:21:26 +0100

    Add metadata from article schema http://schema.org/Article

--- single.php
+++ single.php
@@ -4,10 +4,12 @@
 ?>
 <section id="main">
 <?php if (have_posts()) { while (have_posts()) { the_post(); ?>
-	<article>
+	<article itemscope itemtype="http://schema.org/Article">
 		<header>
-			<h1 class="title"><?php the_title(); ?></h1>
-			<div class="date"><?php the_time('j. n. Y') ?></div>
+			<h1 class="title" itemprop="name"><?php the_title(); ?></h1>
+			<div class="author" itemprop="author" style="display: none;"><?php echo get_the_author_firstname().' '.get_the_author_lastname(); ?></div>
+			<div class="permalink" itemprop="url" style="display: none;"><?php the_permalink(); ?></div>
+			<div class="date" temprop="datePublished"><?php the_time('j. n. Y') ?></div>
 			<div class="clear"></div>
 			<div class="category"><span class="desc">Kategorie: </span><?php the_category(', '); ?></div>
 			<div class="tags"><span class="desc">Štítky: </span><?php the_tags('', ', ', ''); ?></div>
